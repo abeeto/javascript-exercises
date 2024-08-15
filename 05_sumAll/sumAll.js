f88a5a95a7e2e2cc1f011ac721339f1edf6750cc
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2024-08-15 12:27:47 +0100

    Solve for ex05-sumAll using for loop and arrow functions
    * uses two arrow functions to find which arg was upper and lower bound
      ALT could have used Array.from(arguments) to then sort arguments
    * if statement to catch errors
    * for loop to init at lower bound to upper bound, keep summing up
      and return the sum as output

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    // between x and y, sum each one and return
+    // use for loop to be initialized at x keep going till y
+    if (!(Number.isInteger(x) && Number.isInteger(y)) || x < 0 || y < 0 ){
+        return "ERROR";
+    }
+    let sum = 0;
+    let lowerNum = (x, y) => x <= y ? x : y;
+    let upperNum = (x, y) => x <= y ? y : x;
+
+    for (let i = lowerNum(x, y); i <= upperNum(x, y);i++){
+        sum += i;
+    }
+    return sum;
 
 };
 
