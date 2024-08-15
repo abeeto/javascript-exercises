06f20375679c0317af9877869d3a2ab6cc35a826
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2024-08-15 14:54:48 +0100

    Solve 07_tempConversion using formulas for conversion * use tenary operation to see if calculated val is int * if int, return as is, else use methods to convert to 1 d.p number

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  toCels = (fahr - 32)/9 * 5;
+  return Number.isInteger(toCels) ? toCels : Number.parseFloat(toCels.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  toFahr = (cels)/5 * 9 + 32;
+  return Number.isInteger(toFahr) ? toFahr : Number.parseFloat(toFahr.toFixed(1));
 };
 
 // Do not edit below this line
