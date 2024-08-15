7db9dcaeaa30e07ecd6744d653303c372acc250c
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2024-08-15 11:01:59 +0100

    Solve ex04_removeFromArray
    * found two ways to take idefinite number of args as an array
      using spread operator(1) or arguments object within function scope
    
    * did this independently just by reading MDN docs - awesome!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...providedVals) {
+    const newCopy = [];
+    // const args = Array.from(arguments);
+    // const providedVals = args.copyWithin(0, 1);
+
+    arr.forEach(element => {
+        providedVals.includes(element) === false && newCopy.push(element);
+    });
+    return newCopy;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
