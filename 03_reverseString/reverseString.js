d7105f1ba3d43daadee1ebe6f66c40caaea61725
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2024-08-15 10:32:31 +0100

    Solve 03_reverseString using decrementing for loop
    * for loop to initialize at last char of strToRev argument
    * charAt() method to retrieve char, append to revString
    * returns revString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(strToRev) {
+    let revString = "";
+    for (let i = strToRev.length -1; i >= 0; i--){
+        revString += strToRev.charAt(i);
+    }
+    return revString;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
