1e20139aad646cfcdb298fbfc3ccac73fb57faf9
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2024-09-06 00:56:57 +0100

    Finds the eldest person from an array of objects which have dates

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const getAge = (person) => {
+        const livedUntil = person.yearOfDeath ||  new Date().getFullYear();
+        return livedUntil - person.yearOfBirth;
+    };
+    const byAge = (personA, personB) => getAge(personA) - getAge(personB);
+    const agesOfPeople = people.sort(byAge);
+    return agesOfPeople.pop();
 };
 
 // Do not edit below this line
