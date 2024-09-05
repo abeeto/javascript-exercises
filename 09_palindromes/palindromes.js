90394fce681a841e6b7e1e20ef22f3fa12ffd24d
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2024-09-05 11:42:50 +0100

    Refactor so finding charLimit and substrings are separate fns

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,18 @@
 const palindromes = function (str) {
+    let [leftSubStr, rightSubStr] = getSubStrs(str, getCharLimit(str))
+    return leftSubStr === rightSubStr;
+};
+
+function getCharLimit(str){
     let charLimit = (str.length)/2;
+    
     if(str.length % 2 === 1){
         charLimit = (str.length -1)/2;
     }
+    return charLimit;
+}
+
+function getSubStrs(str, charLimit){
     let leftSubStr= str.slice(0, charLimit);
     let rightSubStr = str
     .slice(-charLimit)
@@ -10,8 +20,9 @@ const palindromes = function (str) {
     .reverse()
     .join("");
 
-    console.log(leftSubStr, rightSubStr);
-    return leftSubStr === rightSubStr;
-};
+    return [leftSubStr, rightSubStr];
+}
+
+console.log(palindromes('racecar'));
 // Do not edit below this line
 module.exports = palindromes;
