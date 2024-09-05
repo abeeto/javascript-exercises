fe88998d1c40162603f90fd2825165a7633195a5
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2024-09-05 12:23:35 +0100

    Add cleanupStr() to deal with multiple words and punctuation in string

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,21 @@
 const palindromes = function (str) {
-    let [leftSubStr, rightSubStr] = getSubStrs(str, getCharLimit(str))
+    let cleanStr = cleanupStr(str);
+    let [leftSubStr, rightSubStr] = getSubStrs(cleanStr, getCharLimit(cleanStr))
     return leftSubStr === rightSubStr;
 };
+
+function cleanupStr(str){
+    let words = str.split(', ');
+    const removeIfPunctuation = (val) => !([";", ",", ".", "!", "(", ")", "[", "]", " "].includes(val))
+    let noPuncWords = words.map(
+        (word) => word
+        .toLowerCase()
+        .split("")
+        .filter(removeIfPunctuation)
+        .join('')    
+    )
+    return noPuncWords.join('');
+}
 
 function getCharLimit(str){
     let charLimit = (str.length)/2;
@@ -23,6 +37,6 @@ function getSubStrs(str, charLimit){
     return [leftSubStr, rightSubStr];
 }
 
-console.log(palindromes('racecar'));
+console.log(palindromes('A car, a man, a maraca.'));
 // Do not edit below this line
 module.exports = palindromes;
